8afa198dc29f515ace9f3087ba1818f67b6ace97
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-12-04 13:47:34 +0700

    fix :restore app.js config.js and package.json

---
+++ backend/app.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import apiRoutes from './apiRoutes.js';
+import cors from 'cors';
+import { jenisBarangSeeder, barangSeeder, transaksiSeeder } from './utils/seeder.js'; // Replace with the actual path
+
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+app.use('/api', apiRoutes);
+
+// Run the seeders
+runSeeders();
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    async () => {
+      try {
+        // Run seeders
+        await jenisBarangSeeder();
+        await barangSeeder();
+        await transaksiSeeder();
+
+        console.log('All seeders executed successfully!');
+      } catch (error) {
+        console.error('Error running seeders:', error);
+      }
+    }
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT|3333;
+
+export const mongoDBURL = process.env.MONGODB_URI ||'mongodb://localhost:27017/your-database-name';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
